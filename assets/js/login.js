e1c47daf2b1f7c16c7996b1d2f71b2cbc7c530c4
hanfenqiang <m18806179365@163.com>
2022-09-26 20:51:52 +0800

    user增加了登录注册功能

---
+++ assets/js/login.js
@@ -0,0 +1,19 @@
+$(function (){
+  $('#go2login').on('click',function (){
+    $('.regbox').show()
+    $('.loginbox').hide()
+  })
+
+  $('#go2reg').on('click',function (){
+    $('.regbox').hide()
+    $('.loginbox').show()
+  })
+
+
+
+
+
+
+
+
+})
